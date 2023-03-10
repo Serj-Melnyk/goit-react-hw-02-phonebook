50d71c511748527378950ffff5b60d8afdc810e7
Serj-Melnyk <SerhiiMelnykGit@gmail.com>
2023-03-10 21:30:04 +0200

    add-propTypes

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,14 @@
-
+import PropTypes from "prop-types"
 export const Layout = ({ children }) => {
     
     return (
         <div>
-            <main>{children }</main>
+            <main>{children}</main>
         </div>
 
     )
-}
+};
+
+Layout.propTypes = {
+    children: PropTypes.node.isRequired,
+};
